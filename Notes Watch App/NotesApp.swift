9d3afbe29c95f752e5040393ffa9d92dddce7c84
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-04-26 11:25:32 -0700

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,19 @@
+// NotesApp.swift
+// Notes
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
