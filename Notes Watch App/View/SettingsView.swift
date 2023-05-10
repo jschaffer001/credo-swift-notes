32b776d6c008aa1757b585fc15b71c52126d0f13
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-05-10 08:50:41 -0700

    added a settings view that allows the user to set how many lines of the note they want displayed on the main view. Used a custom binding to implement this.

---
+++ Notes Watch App/View/SettingsView.swift
@@ -0,0 +1,54 @@
+// SettingsView.swift
+// Notes
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    @AppStorage("lineCount") var lineCount: Int = 1
+    @State private var value: Float = 1.0
+    
+    // MARK: - FUNCTION
+    
+    func update() {
+        lineCount = Int(value)
+    }
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack(spacing: 8) {
+            // HEADER
+            HeaderView(title: "Settings")
+            
+            // ACTUAL LINE COUNT
+            Text("Lines: \(lineCount)".uppercased())
+                .fontWeight(.bold)
+            
+            // SLIDER
+            // Slider(value: $value, in: 1...4, step: 1) - Original without custom binding
+            // Custom binding with closures that read and write the binding value
+            Slider(value: Binding(get: {
+                self.value
+            }, set: {(newValue) in
+                self.value = newValue
+                self.update()
+            }), in: 1...4, step: 1)
+                .accentColor(.accentColor)
+            
+        } //: VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
